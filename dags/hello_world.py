a819ee6152971f5286672ac70c348596e41f9cc5
Haziq Noor <haziq.noor@outlook.com>
2025-06-04 00:25:09 +0800

    Add hello world DAG

---
+++ dags/hello_world.py
@@ -0,0 +1,13 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def say_hello():
+    print("Hello from Airflow!")
+
+with DAG("hello_world", start_date=datetime(2023, 1, 1),
+schedule_interval="@daily", catchup=False) as dag:
+    hello_task = PythonOperator(
+        task_id="hello_task",
+        python_callable=say_hello
+    )
